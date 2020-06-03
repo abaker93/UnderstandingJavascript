ffee2b1edd915be52a00155885ffbdde2ee2d54c
Anna Baker <62150513+abaker93@users.noreply.github.com>
2020-06-03 16:16:45 -0400

    Lesson 5

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,13 @@
-document.getElementById('title').innerHTML = 'Displaying Javascript in web pages.';
+// var pageTitle = 'Displaying Javascript in web pages.';
+// document.getElementById('title').innerHTML = pageTitle;
+
+var number1 = 5;
+var Number1 = 10;
+
+var total = number1 + Number1;
+
+console.log(total);
+
+//5 == 5;     TRUE  EQUAL TO
+//5 === 5;    TRUE  EQUAL VALUE AND EQUAL TYPE
+//5 === '5';  FALSE
