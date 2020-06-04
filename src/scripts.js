1060d1fcc93d6d24fbf126eb5c30f9918ca0993e
Anna Baker <62150513+abaker93@users.noreply.github.com>
2020-06-04 10:09:08 -0400

    Lesson 7

--- src/scripts.js
+++ src/scripts.js
@@ -12,8 +12,28 @@
 //5 === 5;    TRUE    EQUAL VALUE AND EQUAL TYPE
 //5 === '5';  FALSE
 
-var x = 10;
-x++;
-x++;
-x--;
-console.log(x);
+// var x = 10;
+// x++;
+// x++;
+// x--;
+// console.log(x);
+
+var firstName = 'Anna';
+var secondName = 'Baker';
+
+var quote = "my name is 'Chris'";
+
+var name = 'Chris';
+var nameLength = name.length;
+
+var paragraph =
+	'\tLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco "laboris nisi ut aliquip" ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate \'velit esse cillum\' dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+console.log(firstName + secondName);
+console.log(quote);
+
+document.write(name + ' is ' + 31);
+
+console.log(paragraph);
+
+console.log(nameLength);
