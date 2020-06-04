46eafeaed0519a5ad001f2455b45925bba9f59af
Anna Baker <62150513+abaker93@users.noreply.github.com>
2020-06-04 10:10:42 -0400

    Lesson 7 cleanup

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,13 @@
+// -----------------------------------------
+//           LESSON 4
+// -----------------------------------------
+
 // var pageTitle = 'Displaying Javascript in web pages.';
 // document.getElementById('title').innerHTML = pageTitle;
+
+// -----------------------------------------
+//           LESSON 5
+// -----------------------------------------
 
 // var number1 = 5;
 // var Number1 = 10;
@@ -12,28 +20,40 @@
 //5 === 5;    TRUE    EQUAL VALUE AND EQUAL TYPE
 //5 === '5';  FALSE
 
+// -----------------------------------------
+//           LESSON 6
+// -----------------------------------------
+
 // var x = 10;
 // x++;
 // x++;
 // x--;
 // console.log(x);
 
-var firstName = 'Anna';
-var secondName = 'Baker';
+// -----------------------------------------
+//           LESSON 7
+// -----------------------------------------
 
-var quote = "my name is 'Chris'";
+// var firstName = 'Anna';
+// var secondName = 'Baker';
 
-var name = 'Chris';
-var nameLength = name.length;
+// var quote = "my name is 'Chris'";
 
-var paragraph =
-	'\tLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco "laboris nisi ut aliquip" ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate \'velit esse cillum\' dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+// var name = 'Chris';
+// var nameLength = name.length;
 
-console.log(firstName + secondName);
-console.log(quote);
+// var paragraph =
+// 	'\tLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco "laboris nisi ut aliquip" ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate \'velit esse cillum\' dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-document.write(name + ' is ' + 31);
+// console.log(firstName + secondName);
+// console.log(quote);
 
-console.log(paragraph);
+// document.write(name + ' is ' + 31);
 
-console.log(nameLength);
+// console.log(paragraph);
+
+// console.log(nameLength);
+
+// -----------------------------------------
+//           LESSON 8
+// -----------------------------------------
